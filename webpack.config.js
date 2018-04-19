7460964892726dea1b87188720fe1618497ef2c4
Shenoy, Prasanth <un3795@corp.Alfacompanies.com>
2018-04-18 22:15:44 -0500

    modified the build process to  transpile typescript code to webpack bundle directly using ts loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,15 @@ var basePath = __dirname;
 module.exports = {
     entry: {
         vendor: ["jquery"],
-        app: "./dist/bootstrapper.js"
+        app: "./src/bootstrapper.ts"
     },
     output: {
         path: path.join(basePath, "build"),
         filename: "bundle.js"
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
     },
     devtool: "source-map",
     module: {
@@ -23,6 +27,10 @@ module.exports = {
                 query: {
                     presets: ["es2015"]
                 }
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
             }
         ]
     },
