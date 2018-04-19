0b709908cc6c5e946d8475a6ed7431b3ff1da30e
Shenoy, Prasanth <un3795@corp.Alfacompanies.com>
2018-04-19 10:49:37 -0500

    added bootstrap 3.3.7 and enabled css bundling  via webpack 3.3.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 var path = require("path");
 var webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin')//this plugin helps to create the html files with the script chunks
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var basePath = __dirname;
 
 module.exports = {
     entry: {
-        vendor: ["jquery"],
+        vendor: ["jquery","bootstrap","bootstrap/dist/css/bootstrap.css","./src/style.css"],
         app: "./src/bootstrapper.ts"
     },
     output: {
@@ -31,10 +32,24 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 loader: "ts-loader"
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+                use: [{
+                    loader: "file-loader"
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
             }
         ]
     },
     plugins: [
+        new ExtractTextPlugin('styles.bundle.css'),
         new webpack.ProgressPlugin(),
         new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
         new webpack.ProvidePlugin({
